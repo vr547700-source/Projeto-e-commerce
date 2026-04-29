0acf9a4f76e954e39ba8a025566b80780ae69bd0
Victor Rafael <vr547700@gmail.com>
2026-04-28 20:09:44 -0400

    Criação da estrutura depastas

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+const App = () =>{
+
+  return (
+    <div className="min-h-screen bg-slate-100 dark:bg-slate-900 flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-slate-900 dark:text-slate-100">
+        Setup OK
+      </h1>
+    </div>
+  )
+}
+
+export default App;
